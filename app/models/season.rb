fe083d821982932af3a6e8290c8ca483712ab9e1
Mark Miranda <notmarkmiranda@gmail.com>
2019-11-15 13:43:32 -0700

    add js to make clickable rows

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,6 +1,8 @@
 class Season < ApplicationRecord
   belongs_to :league
 
+  scope :in_created_order, -> { order(created_at: :asc) }
+
   def activate!
     # TODO: deactivate other seasons?
     update!(active_season: true)
@@ -22,12 +24,16 @@ class Season < ApplicationRecord
     deactivate! && completed!
   end
 
-  def uncompleted!
-    update!(completed: false)
-  end
-
   def not_completed?
     !completed?
+  end
+
+  def number_in_order
+    league.seasons.index(self) + 1
+  end
+
+  def uncompleted!
+    update!(completed: false)
   end
 
   def self.any_active?
