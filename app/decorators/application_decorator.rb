396a35970b07118d16ee5eec32763934441a4c25
Mark Miranda <notmarkmiranda@gmail.com>
2019-11-27 20:48:56 -0700

    Add actual games to season show

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -3,6 +3,10 @@ class ApplicationDecorator < Draper::Decorator
     date.strftime('%B %-e, %Y at %l:%M %p')
   end
 
+  def full_date(date)
+    date.strftime('%B %-e, %Y')
+  end
+
   def date_and_year(date)
     date.strftime('%B %Y')
   end
