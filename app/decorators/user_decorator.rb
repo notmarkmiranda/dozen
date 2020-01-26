b6c8b51189241b0f5aff6259a894d4fa24156964
Mark Miranda <notmarkmiranda@gmail.com>
2020-01-26 13:21:45 -0700

    Finalize stats on user profile

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,12 +2,21 @@ class UserDecorator < ApplicationDecorator
   delegate_all
 
   def administrator_and_member_count_text
-    league_count = user.league_count_by_role
+    league_count = league_count_by_role
     admin_leagues = h.pluralize(league_count[:admin], 'league')
     member_leagues = h.pluralize(league_count[:member], 'league')
     output = h.content_tag :div, "Administrator of #{admin_leagues}", class: 'admin-text text-muted'
     output += h.content_tag :div, "Member of #{member_leagues}", class: 'member-text text-muted'
+
     h.content_tag :div, output, class: 'membership-text'
+  end
+
+  def attendance_text
+    output = h.content_tag :strong, "Attendance: "
+    text = "#{seasons_games_count[:played]} out of #{h.pluralize(seasons_games_count[:total], 'game')}"
+    output += text
+    percentage = " - #{h.number_with_precision(seasons_games_count[:percentage], precision: 1)}%"
+    output += percentage
   end
 
   def full_name_or_email
@@ -17,5 +26,21 @@ class UserDecorator < ApplicationDecorator
   def full_name
     return object.email if object.first_name.blank?
     "#{object.first_name} #{object.last_name}".strip
+  end
+
+  def games_played_text
+    output = h.content_tag :strong, "Games Played: "
+    games_played_count = games_played.count
+    output += games_played_count.to_s
+    output
+  end
+
+  def games_won_text
+    output = h.content_tag :strong, "Games Won: "
+    text = "#{games_won_played_count[:won]} out of #{h.pluralize(games_won_played_count[:played], 'game')}"
+    output += text
+    percentage = " - #{h.number_with_precision(games_won_played_count[:percentage], precision: 1)}%"
+    output += percentage
+    output
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,21 @@ class User < ApplicationRecord
     memberships.where(role: 1).map(&:league)
   end
 
+  def games_played
+    Game.joins(:players).where('players.user_id = ?', id)
+  end
+
+  def games_won_played_count
+    games_won = games_played.joins(:players).where('players.finishing_place = ?', 1).count
+    games_played_count = games_played.count
+    percentage = games_played_count.zero? ? 0.0 : (games_won / games_played_count.to_f) * 100
+    { 
+      played: games_played_count, 
+      won: games_won, 
+      percentage: percentage
+    }
+  end
+  
   def league_count_by_role
     { admin: admin_leagues.count, member: member_leagues.count }
   end
@@ -23,6 +38,18 @@ class User < ApplicationRecord
 
   def games_in_reverse_date_order(limit=nil)
     games_in_order(limit: limit, order: :desc).decorate
+  end
+
+  def seasons_games_count
+    seasons = Season.joins(:players).where('players.user_id = ?', id)
+    games_played_count = games_played.count
+    total_games = seasons.sum(&:games_count)
+    percentage = total_games.zero? ? 0.0 : (games_played_count / total_games.to_f) * 100
+    {
+      played: games_played_count,
+      total: total_games,
+      percentage: percentage
+    }
   end
 
   private
