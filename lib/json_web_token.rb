ccc07870220de144b83431e96d0fd1f815c12872
Mark Miranda <notmarkmiranda@gmail.com>
2020-03-15 14:35:59 -0600

    change references to #credentials from #secrets

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,10 +1,12 @@
 class JsonWebToken
+  SECRET_KEY = Rails.application.credentials.secret_key_base
+  
   def self.encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    decoded = JWT.decode(token, SECRET_KEY)[0]
     return HashWithIndifferentAccess.new(decoded)
   rescue
     nil
