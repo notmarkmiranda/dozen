55384a828e31eb981099e4b795e7d42a24993490
Michael Cassano <mcassano@squaretrade.com>
2020-09-18 12:51:40 -0600

    support for running in a Docker container

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /myapp/tmp/pids/server.pid
+
+bundle exec rails db:migrate RAILS_ENV=development
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
