60029ec26e8b07b528a0db1b009dfacf60a174f5
Makr Miranda <github@markmiranda.email>
2020-12-31 23:15:48 -0700

    Add net earnings to season#show

---
+++ spec/features/seasons/season_has_net_earnings_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "Season has net earnings", type: :feature do
+  describe "For a season with a completed game" do
+    let(:game) { create(:game_with_players, buy_in: 100) }
+    let(:season) { game.season }
+    let(:user) { game.players.last.user }
+
+    before do 
+      login_as(user, scope: :user)
+    end
+
+    it "shows net earnings on a season show" do
+      visit season_path(season)
+
+      expect(page).to have_content("$150.00")
+      expect(page).to have_content("$50.00")
+      expect(page).to have_content("$0.00")
+    end
+  end
+end
