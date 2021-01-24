ff1d8a630cbd3e37b67674993ff051ce54b23346
Michael Cassano <mcassano@gmail.com>
2021-01-23 19:36:54 -0700

    remove the test setup

--- spec/features/sessions/visitor_can_click_menu_spec.rb
+++ spec/features/sessions/visitor_can_click_menu_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 describe 'Visitor can click menu', type: :feature do
-  before(:all) do
+  it 'attempt to click the menu' do
     Capybara.default_driver = :selenium
+
+    visit "/"
 
     # Resize to cause bootstrap to show the menu
     page.driver.browser.manage.window.resize_to(400,200)
-  end
-
-  it 'attempt to click the menu' do
-    visit "/"
 
     # The page should not show the menu until we click the menu
     expect(page).to have_no_css '.navbar-collapse'
